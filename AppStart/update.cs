725bf99bb40a583b5a26fdb969ed4154330a8cd0
sjin9696 <sjin9696@gmail.com>
2021-10-06 01:12:28 +0900

    첫 커밋
    솔루션최초작성,
    AppStart프로젝트추가
    update.cs 생성, webdata.cs생성
    웹에 접근해서 로컬로다운받는기능
    파일비교해서 높은버전비교하는 클래스

---
+++ AppStart/update.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//2021-10-06 송진영
+//내려받은 UM.txt에 있는 버전 숫자와 로컬에 있는 UM.txt파일의 버전 숫자를 비교해서 
+//올려둔 버전이 더 최신이면 업데이트를 실행하는 역할
+//
+namespace AppStart
+{
+    class update
+    {
+    }
+}
---
+++ AppStart/webdata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//2021-10-06 송진영
+//웹에 올려둔 UM.txt 파일과 UM.zip파일을 로컬로 다운로드 받는 역할을 하는 클래스
+namespace AppStart
+{
+    class webdata
+    {
+    }
+}
